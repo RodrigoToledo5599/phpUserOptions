ba646c53e53d5c7ad4ca76da16c7392f339c997d
RodrigoToledo5599 <rodrigotmt89@gmail.com>
2023-12-22 14:44:18 -0300

    estilizando um pouco a pagina

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,10 +4,37 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Options</title>
+    <link rel="stylesheet" href="{{ asset('/css/welcome.css') }}"/>
+    <style>
+        body{
+            width:100%;
+            height:100%;
+            background-color: black;
+            color:white;
+            display:flex;
+            align-items: center;
+            justify-content: center;
+            
+        }
+        a{
+            color:white;
+            text-decoration: none;
+        }
+        .options{
+            margin-top: 10%;
+            font-family:serif;
+            font-size: 50px;
+        }
+
+    </style>
+    
 </head>
 <body>
-    <a href="{{ url('/createAcc') }}">Criar Conta</a><br><br><br>
-    <a href="{{ url('/auth') }}">Logar Conta</a>
-</body>
+    <div class="options">
+        <a href="{{ url('/createAcc') }}">Criar Conta</a><br><br><br>
+        <a href="{{ url('/auth') }}">Logar Conta</a>
+    </div>
+    </body>
+
 </html>
