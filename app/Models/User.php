07b88419eb48d85a6d302ad2a7c10b933cfcb9f2
RodrigoToledo5599 <rodrigotmt89@gmail.com>
2023-12-27 11:39:37 -0300

    acidentally changed the user model to a task and now im changing it back

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,17 +12,34 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    public $title;
-    public $description;
-    public $user_id;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
 
-    protected $fillable = [];
-
-    
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
     protected $hidden = [
-        'id',
+        'password',
         'remember_token',
     ];
 
-    
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
 }
